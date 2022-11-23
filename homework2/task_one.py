f1c5215db1fc3a1da3043285e2d4ed2892eab8b9
Yury Zhurok <zhurok.yuriy@mail.ru>
2022-11-23 21:33:25 +0100

    Fixed bugs of the flake8

--- homework2/task_one.py
+++ homework2/task_one.py
@@ -1,7 +1,9 @@
+from math import sqrt
 coordinate_x = float(input("Введите координату x: "))
 coordinate_y = float(input("Введите координату y: "))
 coordinate_x1 = float(input("Введите координату x1: "))
 coordinate_y1 = float(input("Введите координату y1: "))
-from math import sqrt
-distance = sqrt((coordinate_x1 - coordinate_x)**2 + (coordinate_y1 - coordinate_y)**2)
+distance = sqrt(
+    (coordinate_x1 - coordinate_x)**2 + (coordinate_y1 - coordinate_y)**2
+    )
 print(f"Расстояние между точками: {distance}")
