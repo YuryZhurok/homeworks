90db59a54819d9ac9adbe0716366b06c9113cfd8
Yury Zhurok <zhurok.yuriy@mail.ru>
2022-11-29 19:31:54 +0100

    Fixed bugs and improved code

--- homework3/task_three.py
+++ homework3/task_three.py
@@ -1,6 +1,6 @@
 import random
-spisok = random.sample(range(-100, 100), 10)
-print(spisok)
-spisok[2] = random.randint(-100, 100)
-del spisok[6]
-print(spisok)
+list = random.sample(range(-100, 100), 10)
+print(list)
+list[2] = random.randint(-100, 100)
+del list[6]
+print(list)
