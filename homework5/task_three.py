036dfd2bb65fecd08d4d4fb0e6b30a2926d08916
Yury Zhurok <zhurok.yuriy@mail.ru>
2022-12-06 19:30:41 +0100

    Fixed bugs

--- homework5/task_three.py
+++ homework5/task_three.py
@@ -1,6 +1,8 @@
 with open('text.txt') as text_txt:
     song = text_txt.read()
     user_letter = input("Enter any english letter: ")
-    song.lower()
-    quantity = song.count(user_letter)
+    song = song.lower()
+    user_letter = user_letter.lower()
+    for line in song:
+        quantity = song.count(user_letter)
     print(f"Результат: буква встречается {quantity} раз")
