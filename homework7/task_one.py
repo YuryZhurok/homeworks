8827830fde8c16fbd71a6a6c2d23c60c6cc481cd
Yury Zhurok <zhurok.yuriy@mail.ru>
2022-12-11 17:30:54 +0100

    Fixed bugs

--- homework7/task_one.py
+++ homework7/task_one.py
@@ -1,9 +1,8 @@
 def get_winners(results):
-    number_of_winners = [133, 32, 445, 330, 564]
-    new_numbers = list(enumerate(number_of_winners, start=1))
+    new_numbers = list(enumerate(results, start=1))
     new_numbers.sort(key=lambda x: x[1], reverse=True)
     results = [new_numbers[0][0], new_numbers[1][0], new_numbers[2][0]]
     return results
 
 
-print(get_winners([133, 32, 445, 330, 564]))
+print(get_winners([533, 32, 45, 330, 564]))
